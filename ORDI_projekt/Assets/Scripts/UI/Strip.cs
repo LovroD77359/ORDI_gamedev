905f6b18c1442799cb96ab06335e2b7ed5fbdd6b
janina4000 <jm54789@fer.hr>
2025-01-28 10:04:45 +0100

    Skripta Strip
    Stara skripta Strip je preimenovana u Strip_stari
    Nova skripta trenutačno zahtjeva 5 gameObjecta, tj. stranica
    Za sada skripta samo flipa između stranica, pokušat ću dodati da se slike na svakoj stranici izmjenjuju.

--- ORDI_projekt/Assets/Scripts/UI/Strip.cs
+++ ORDI_projekt/Assets/Scripts/UI/Strip.cs
@@ -1,27 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Strip : MonoBehaviour
 {
+    public GameObject Page1;
+    public GameObject Page2;
+    public GameObject Page3;
+    public GameObject Page4;
+    public GameObject Page5;
+
+    private int pageNumber = 0;
+    private GameObject[] pages;
+    private RawImage[][] rawImagesArray;
+
+    // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(StripCoroutine());
+        Page1.gameObject.SetActive(true);
+        Page2.gameObject.SetActive(false);
+        Page3.gameObject.SetActive(false);
+        Page4.gameObject.SetActive(false);
+        Page5.gameObject.SetActive(false);
+ 
+        pages = new GameObject[] { Page1, Page2, Page3, Page4, Page5};
+
+        rawImagesArray = new RawImage[pages.Length][];
+        for (int i = 0; i < pages.Length; i++)
+        {
+            rawImagesArray[i] = pages[i].GetComponentsInChildren<RawImage>();
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey) { 
-            Scene nextScene = SceneManager.GetActiveScene();
-            SceneManager.LoadSceneAsync(nextScene.buildIndex + 1);
+        if (Input.anyKeyDown) 
+        {
+            if (pageNumber < pages.Length-1) 
+            {
+                pageNumber++;
+                flip();
+            }
         }
     }
 
-    IEnumerator StripCoroutine() 
+    private void flip() 
     {
-        yield return new WaitForSeconds(5);
+        for (int i = 0; i < pages.Length; i++) 
+        {
+            if (i == pageNumber)
+            {
+                pages[i].gameObject.SetActive(true);
+            }
+            else 
+            {
+                pages[i].gameObject.SetActive(false);
+            }
+            
+        }
+        
     }
+
+
 }
